58e92cf477d9a5b968283e88d2990ccdbfa43714
Satyam <128127471+Raichu70@users.noreply.github.com>
2023-05-25 01:00:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -54,7 +54,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 # For customized or modified Repository
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/kshitijraj960/mfia",
+    "https://github.com/TEAM-UNK/mfia-testing1",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 
